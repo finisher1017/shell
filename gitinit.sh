31c0d0e29434c3d4e2847b711dced5df32fcd9f3
Jonathan Seubert <finisher1017@gmail.com>
2019-05-11 22:47:27 -0400

    second commit

--- gitinit.sh
+++ gitinit.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 commitComment=$1
 origin=$2
+dir=$(pwd)
 git init
-git add .
+git add $dir
 git commit -m "${commitComment}"
-git remote add origin "${origin}"
-git push -u origin master
+# git remote add origin "${origin}"
+git push
