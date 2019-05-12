5b1c28a7cda8206e237c2382be70d9cff6935ea2
Jonathan Seubert <finisher1017@gmail.com>
2019-05-11 22:30:15 -0400

    first commit

--- gitinit.sh
+++ gitinit.sh
@@ -3,6 +3,6 @@ commitComment=$1
 origin=$2
 git init
 git add .
-git commit -m "${commitComment}""
-git remote add origin "${origin}""
+git commit -m "${commitComment}"
+git remote add origin "${origin}"
 git push -u origin master
